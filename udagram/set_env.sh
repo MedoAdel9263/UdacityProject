5232d8d826ccc24dfada32267810902b0e653129
Mohamed Adel <mohamed.adel@Digitizeflow.com>
2022-12-06 03:29:15 +0200

    init

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Passw0rd_13462531133
+export POSTGRES_HOST=database-1.cbegjmn3bd4o.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::mymadelbucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://contact-api-dev.eba-brmau8q8.us-east-1.elasticbeanstalk.com/
